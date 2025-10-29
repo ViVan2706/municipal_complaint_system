13451aea448a32d081e6a64f333b5d87c3234450
nikhil <nikhiljain.10905@gmail.com>
2025-10-29 14:01:04 +0530

    database name changed

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = $_ENV['DB_HOST'] ?? 'localhost'; 
 $username = $_ENV['DB_USERNAME'] ?? 'root';    
 $password = $_ENV['DB_PASSWORD'] ?? ''; 
-$dbname = $_ENV['DB_NAME'] ?? 'municipal';
+$dbname = $_ENV['DB_NAME'] ?? 'complaint_system';
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     error_log("Connection Failed: " . $conn->connect_error);
